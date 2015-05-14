119ca38abbdefe20e7b471421841d61adde9831c
protreptic <bukhal.ps@gmail.com>
2015-05-14 16:41:56 +0400

    0

---
+++ src/org/javaprotrepticon/android/cbrinformer/storage/model/Store.java
@@ -0,0 +1,9 @@
+package org.javaprotrepticon.android.cbrinformer.storage.model;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable
+public class Store {
+
+}
